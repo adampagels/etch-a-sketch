9e1fd55c09238c37d12661f96d5e5ab6a1e0e1ee
Adam Pagels <pagels@hotmail.ca>
2019-11-02 12:57:16 -0400

    Add function to allow user to select grid size

--- js/index.js
+++ js/index.js
@@ -16,4 +16,28 @@ function createGrid() {
     }
 }
 
+function createNewGrid() {
+    let userInput = Number(prompt('Select new grid size', 'Example: "64"'))
+    console.log(typeof userInput);
+    if (userInput < 1) {
+        createGrid();
+        alert('Please enter a number greater than 0');
+    } else if (!isNaN(userInput)) {
+        let squareDimensions = (100 / userInput)
+        for (i = 0; i < (userInput * userInput); i++) {
+            const gridSquare = document.createElement('div')
+            gridSquare.classList.add('grid-square')
+            gridContainer.appendChild(gridSquare)
+            gridSquare.style.cssText = `height: ${squareDimensions}%;
+        width: ${squareDimensions}%;`
+            gridSquare.addEventListener('mouseover', () => {
+                gridSquare.classList.add('hover');
+            });
+        }
+    } else {
+        createGrid();
+        alert('Please enter a number');
+    }
+}
+
 createGrid();
