49d410d14cf588d0b55e5cfe66eaecd4f6d5b665
Adam Pagels <pagels@hotmail.ca>
2019-11-02 12:25:20 -0400

    Add function to create landing page grid

--- js/index.js
+++ js/index.js
@@ -3,3 +3,14 @@ let container = document.querySelector('#container');
 let gridContainer = document.createElement('div');
 gridContainer.classList.add('grid-container');
 container.appendChild(gridContainer);
+
+function createGrid() {
+    for (i = 0; i < 256; i++) {
+        const gridSquare = document.createElement('div')
+        gridSquare.classList.add('grid-square')
+        gridContainer.appendChild(gridSquare);
+        gridSquare.style.cssText = 'height: 6%; width: 6%'
+    }
+}
+
+createGrid();
